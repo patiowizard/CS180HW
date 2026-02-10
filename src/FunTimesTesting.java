eebdb9eeacd2522af4d23c98cbc6a336e7138cbf
patiowizard <andresem071@gmail.com>
2026-02-09 21:45:12 -0500

    boompas commit

--- src/FunTimesTesting.java
+++ src/FunTimesTesting.java
@@ -5,7 +5,7 @@ public class FunTimesTesting
         static void main() {
             Scanner sc = new Scanner(System.in);
 
-            System.out.println("worbles? yes or no?");
+            System.out.println("worbles? yes or no? HAHAHAHA boompas");
             String worbles = sc.nextLine();
 
             if (worbles.toLowerCase().contains("yes")) {
