623c008f1fb6a4dbfd3fe6a70b14399ce99197f8
patiowizard <andresem071@gmail.com>
2026-02-12 15:25:21 -0500

    2/12 1stSave of HW04Challenge

---
+++ src/DuckDuckSquirrel.java
@@ -0,0 +1,19 @@
+//Javadoc Here
+public class DuckDuckSquirrel {
+
+    private final static String WELCOME_MESSAGE = "Another semester another issue... " +
+            "Guess we better find those squirrels!";
+    private final static String ENTRY_PROMPT = "Enter the String to Search:";
+    private final static String SHUT_DOWN = "Hopefully that's all of them... Thanks for the help!";
+    private final static String SEG_FAULT = "You know the term BUG comes from a literal bug " +
+            "that got into a computer and caused an issue?\n" +
+            "Maybe we should change it to ACORN... Alright lets try again...";
+    private final static String LAZY_USER = "Typing issue? Come on, this won't fix itself.";
+
+    //todo change this string to be correct
+    private final static String VALID_OUTPUT_DIALOG = "Cycles: 0 Ducks: 0 Squirrels: 0 DSR: 0.000 SCR: 0.000";
+
+    //todo implement your program here
+
+
+}
